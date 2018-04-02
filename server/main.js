5d52c5dc8ea2edc743e0705433afaf82809e3152
eastindi <developer.eastindi@gmail.com>
2018-04-01 23:45:50 -0700

    wip

--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  //SSL('C:/data/projects/meteor/ade2a8c05dfb4d1f8e8c1c01f587e343/private/server.key','C:/data/projects/meteor/ade2a8c05dfb4d1f8e8c1c01f587e343/private/server.crt', 443);
 });
