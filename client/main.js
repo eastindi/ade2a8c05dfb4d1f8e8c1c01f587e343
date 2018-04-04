570055ca8a7a15194198238a7d787b7d95c77793
Sanjay Kumar <kumars@pf.local>
2018-04-04 01:09:21 -0700

    stripe create customer

--- client/main.js
+++ client/main.js
@@ -7,23 +7,11 @@ import {
 
 import './main.html';
 import './views/payment/payment_account.js';
-
-// Template.body.helpers({
-//     customer_create() {
-//         var cid = document.getElementById("opt_customer").value;
-//         alert(cid);
-//    }
-// });
-
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
+import { error } from 'util';
 
 Template.customer_create.events({
     'click #btn_customer_create': function () {
-        var cust = require('../imports/api/payment/customer.js');
-        var c = cust.create(document.getElementById("opt_customer").value);
+    var r = Meteor.call('createStripeCustomer', document.getElementById("opt_customer").value);
+    console.log(r);
     }
 });
